ef38b7c331e250a6e4188dfa42d46e9d07bcddad
Aman <amant2312@gmail.com>
2024-03-04 14:52:56 +0530

    Add project files.

---
+++ TwoDecksBlazor/Card.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoDecksBlazor
+{
+    internal class Card
+    {
+        public values Value { get; private set; }
+        public suits Suit { get; private set; }
+        /*public string Name { get { return $"{Value} of {Suit}"; } }*/
+        public Card(values value, suits suit)
+        {
+            Value = value;
+            Suit = suit;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value} of {Suit}";
+        }
+
+
+    }
+}
---
+++ TwoDecksBlazor/CardComparerByValue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoDecksBlazor
+{
+    internal class CardComparerByValue: IComparer<Card>
+    {
+        public int Compare(Card a, Card b)
+        {
+            
+            if (a.Suit < b.Suit) return -1;
+            if (a.Suit > b.Suit) return 1;
+            if(a.Value < b.Value) return -1;
+            if(a.Value > b.Value)return 1;
+            return 0;
+        }
+    }
+}
